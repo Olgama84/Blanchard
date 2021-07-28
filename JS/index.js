41e1b31b6945d75fff4724ae5a0e417ed73e1b88
Olga Ma <78204466+Olgama84@users.noreply.github.com>
2021-07-28 20:39:55 +0400

    updated header and hero sections

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
 // Burger menu
 window.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#burger').addEventListener('click', (e) =>{
-        e.currentTarget.classList.toggle('btn-burger--active')
+        e.currentTarget.classList.toggle('burger__btn-active')
     })
     document.querySelector('#burger').addEventListener('click', function(){
-        document.querySelector('#burger-menu').classList.toggle('is-active')
+        document.querySelector('#burger-menu').classList.toggle('menu-is-active')
     })    
 })
 
 // Dropdown menu
-let menuBtn = document.querySelector('.btn');
+let menuBtn = document.querySelector('.btn1');
 let menu = document.getElementById("myDropdown");
 let toggleMenu = function() {
     menu.classList.toggle('show');
@@ -32,7 +32,7 @@ document.addEventListener('click', function(e) {
 let menuBtn2 = document.querySelector('.btn2');
 let menu2 = document.getElementById("myDropdown2")
 let toggleMenu2 = function() {
-    menu2.classList.toggle('show2');
+    menu2.classList.toggle('show');
 }
 menuBtn2.addEventListener('click', function(e) {
     e.stopPropagation();
@@ -42,7 +42,7 @@ document.addEventListener('click', function(e) {
     let target = e.target;
     let its_menu = target == menu2 || menu2.contains(target);
     let its_menuBtn = target == menuBtn2;
-    let menu_is_active = menu2.classList.contains('show2');
+    let menu_is_active = menu2.classList.contains('show');
 
     if (!its_menu && !its_menuBtn && menu_is_active) {
         toggleMenu2();
@@ -52,7 +52,7 @@ document.addEventListener('click', function(e) {
 let menuBtn3 = document.querySelector('.btn3');
 let menu3 = document.getElementById("myDropdown3")
 let toggleMenu3 = function() {
-    menu3.classList.toggle('show3');
+    menu3.classList.toggle('show');
 }
 menuBtn3.addEventListener('click', function(e) {
     e.stopPropagation();
@@ -62,7 +62,7 @@ document.addEventListener('click', function(e) {
     let target = e.target;
     let its_menu = target == menu3 || menu3.contains(target);
     let its_menuBtn = target == menuBtn3;
-    let menu_is_active = menu3.classList.contains('show3');
+    let menu_is_active = menu3.classList.contains('show');
 
     if (!its_menu && !its_menuBtn && menu_is_active) {
         toggleMenu3();
@@ -72,7 +72,7 @@ document.addEventListener('click', function(e) {
 let menuBtn4 = document.querySelector('.btn4');
 let menu4 = document.getElementById("myDropdown4")
 let toggleMenu4 = function() {
-    menu4.classList.toggle('show4');
+    menu4.classList.toggle('show');
 }
 menuBtn4.addEventListener('click', function(e) {
     e.stopPropagation();
@@ -82,7 +82,7 @@ document.addEventListener('click', function(e) {
     let target = e.target;
     let its_menu = target == menu4 || menu4.contains(target);
     let its_menuBtn = target == menuBtn4;
-    let menu_is_active = menu4.classList.contains('show4');
+    let menu_is_active = menu4.classList.contains('show');
 
     if (!its_menu && !its_menuBtn && menu_is_active) {
         toggleMenu4();
@@ -92,7 +92,7 @@ document.addEventListener('click', function(e) {
 let menuBtn5 = document.querySelector('.btn5');
 let menu5 = document.getElementById("myDropdown5")
 let toggleMenu5 = function() {
-    menu5.classList.toggle('show5');
+    menu5.classList.toggle('show');
 }
 menuBtn5.addEventListener('click', function(e) {
     e.stopPropagation();
@@ -102,7 +102,7 @@ document.addEventListener('click', function(e) {
     let target = e.target;
     let its_menu = target == menu5 || menu5.contains(target);
     let its_menuBtn = target == menuBtn5;
-    let menu_is_active = menu5.classList.contains('show5');
+    let menu_is_active = menu5.classList.contains('show');
 
     if (!its_menu && !its_menuBtn && menu_is_active) {
         toggleMenu5();
